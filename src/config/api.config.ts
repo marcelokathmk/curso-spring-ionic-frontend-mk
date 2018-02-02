c082b592491c56787831396be558b425f364790e
Marcelo Kath <marcelokathmk@gmail.com>
2018-02-02 14:39:29 -0200

    Mostrando as categorias

--- src/config/api.config.ts
+++ src/config/api.config.ts
@@ -1,3 +1,5 @@
 export const API_CONFIG = {
-    base_url : "http://localhost:8080"
+    base_url : "http://localhost:8080",
+    bucket_base_url : "https://s3.amazonaws.com/curso-spring",
+    img_extension : ".jpg"
 }
