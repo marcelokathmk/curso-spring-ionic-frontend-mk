61f4b02ec9749972d16619add66d45517c602cc9
Marcelo Kath <marcelokathmk@gmail.com>
2018-02-08 10:02:06 -0200

    Extraindo email do token

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -4,9 +4,12 @@ import { HttpClient } from "@angular/common/http";
 import { API_CONFIG } from "../config/api.config";
 import { LocalUser } from "../models/local-user";
 import { StorageService } from "./storage.service";
+import { JwtHelper } from "angular2-jwt";
 
 @Injectable()
 export class AuthService{
+
+    jwtHelper: JwtHelper = new JwtHelper();
 
     constructor(public http: HttpClient, public storage: StorageService){
 
@@ -24,7 +27,8 @@ export class AuthService{
     public sucessfullLogin(authorizationValue : String){
         let token = authorizationValue.substring(7);
         let user : LocalUser = {
-            token: token
+            token: token,
+            email: this.jwtHelper.decodeToken(token).sub
         };
         this.storage.setLocalUser(user);
     }
